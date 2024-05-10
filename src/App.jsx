f6da2a13c2cba22dc3c5b1db43917470e420feeb
PRRInc <prrincdev@gmail.com>
2024-05-09 22:01:44 -0400

    API Setup, .env, and file path fixes for Components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Components
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+// Routes
+import Home from "./Components/Home";
+import AboutUs from "./Components/AboutUs";
+import ShowPage from "./Components/ShowPage";
 
 function App() {
-
   return (
-    <>
-
-        
-    </>
-  )
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/aboutus" element={<AboutUs />}/>
+          <Route path="/:id" element={<ShowPage />}/>
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
