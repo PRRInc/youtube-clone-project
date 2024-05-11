c996c71f788b536ebc960c7f7c935c02246346aa
PRRInc <prrincdev@gmail.com>
2024-05-11 17:52:16 -0400

    Import useEffect, useState, Link, useParams, useNavigate, DeleteComment, getComment

--- src/Components/ShowPage.jsx
+++ src/Components/ShowPage.jsx
@@ -1,4 +1,7 @@
-
+import { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom"
+import { DeleteComment, getComment } from "../api/fetch"
 
 export default function ShowPage() {
+
 }
