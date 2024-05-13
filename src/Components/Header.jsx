9f32b073b2820aa3b55f5d5f6ca75d593a07b4ff
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-13 00:01:19 -0400

    This adds a logo and text input in the header component with styling.  no functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,15 @@ import "./Header.css";
 
 export default function Header () {
     return (
-        <div className="topbar">
-        </div>
+        <>
+            <div id="topbar-logo-container">
+                <img src="assets/yt_logo_cmyk_light.svg" alt="Youtube Text Logo"/>
+            </div>
+            <div id="query-container">
+                <input type="text" name="query" id="query-box" placeholder="Search"/>
+                <img src="assets/search.svg" alt="search icon"/>
+            </div>
+    
+        </>
     )
 }
