6c01a4fbb36fb49ebac0e507a2cc6812bc342d96
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-15 12:33:39 -0400

    adds html structure to home component.  functionality incomplete.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,50 @@
+import topVideoCategories from "../data/topvideocategories";
 import "./Home.css";
 
 export default function Home() {
+
+    const API_KEY = import.meta.env.VITE_API_KEY
+    let categorizedVideoArr = [];
+    let categoryNumVals = Object.values(topVideoCategories);
+
+    function getVideosByCatgeory(category){
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoCategoryId=${category}&key=${API_KEY}`)
+        .then( (response) => response.json )
+        .then( res => {
+            categorizedVideoArr = res.item;
+            return categorizedVideoArr})
+    }
     return (
-        <>
+        <> 
+          <div className="category-container">
+                <h3>Category</h3>
+                    { categoryNumVals.map( val => {
+                        getVideosByCatgeory(val).map( el =>
+                            {
+                                return ( 
+                                    <div className="category-video-list">
+                                        <div className="video-thumbnail">
+                                            <div>
+                                                <img src="{ result.snippet.thumbnails.small.url }" alt="video thumbnail"/>
+                                            </div>
+                                            <div className="category-video-card">
+                                                <h4 className="category-video-title">{ el.snippet.title }</h4>
+                                                <div className="details-container">
+                                                    <p className="category-video-channel">{ el.snippet.channelTitle }</p>
+                                                    <div className="category-video-channel">
+                                                        <p className="category-video-views">521,600 Views</p>
+                                                        <p className="category-video-date">{ el.snippet.publishedAt }</p>
+                                                    </div> 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        );
+                        })
+                    }
+            </div>
         </>
     )
 }
---
+++ src/data/topvideocategories.js
@@ -0,0 +1,9 @@
+const topVideoCategories = {
+    comedy: "34",
+    gaming: "20",
+    sports: "17",
+    people: "22",
+    music: "10"
+}
+
+export default topVideoCategories
