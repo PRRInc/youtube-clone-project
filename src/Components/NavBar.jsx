65bddc8956e7175ebd0dc3fe143685cd410e2fb8
lynnjaco <jacobs.lynnette@gmail.com>
2024-05-15 15:11:11 -0400

    this code modifies the styling of the navbar/sidebar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,33 +3,30 @@ import "./NavBar.css";
 
 export default function NavBar() {
     return (
-        <div id="sidebar-container">
-            
+        <div id="sidebar-container">  
             <div id="sidebar-elements">
                 <h5>Site</h5>
                 <div className="row sidebar-link">
                     <img src="assets/noun-home-6843542.svg" alt="home icon" />
-                    <Link to="/">Home</Link>
+                    <Link to="/" className="nav-link">Home</Link>
                 </div>
                 <div className="row sidebar-link">
                     <img src="assets/question.svg" alt="home icon" />
-                    <Link to="/aboutus">About</Link>
+                    <Link to="/aboutus" className="nav-link">About</Link>
                 </div>
-                <hr />
                 <h5>User</h5>
                 <div className="row sidebar-link">
                     <img src="assets/heart.svg" alt="home icon" />
-                    <Link to="/favorites">Favorites</Link>
+                    <Link to="/favorites" className="nav-link">Favorites</Link>
                 </div>
                 <div className="row sidebar-link">
                     <img src="assets/clock.svg" alt="home icon" />
-                    <Link to="/history">History</Link>
-                </div>
-                <div id="logo-container">
-                    <img id="youtube-logo" src="assets/yt_icon_rgb.png" alt="youtube logo"/>
+                    <Link to="/history" className="nav-link">History</Link>
                 </div>
             </div>
-            
+            <div id="logo-container">
+                <img id="youtube-logo" src="assets/yt_icon_rgb.png" alt="youtube logo"/>
+            </div>
         </div>
     )
 }
